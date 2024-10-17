a4466e07fe150251eec4f5de18b9b8b5ed7117e8
Olga Bachurina <bachuri12@ukr.net>
2024-10-17 14:52:33 +0300

    Модель для категорий товаров. Содержит данные о категориях, к которым относятся продукты, помогает структурировать каталог.

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using ServerABBKCOSY.Models;
+
+namespace ServerABBKCOSY
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int? ParentCategoryId { get; set; }  // внешний ключ на родительскую категорию
+
+        // Связь "один ко многим" с продуктами
+        public ICollection<Product> Products { get; set; }
+
+        // Самоссылающаяся связь для подкатегорий
+        public Category ParentCategory { get; set; }
+        public ICollection<Category> SubCategories { get; set; }
+    }
+}
