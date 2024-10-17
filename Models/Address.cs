a28fadaf922e733f93af8ded11c1ecc74a9f9673
Olga Bachurina <bachuri12@ukr.net>
2024-10-17 14:48:58 +0300

    Модель для хранения данных об адресе пользователя. Хранит информацию о городе, улице и почтовом индексе, которая используется для доставки или оплаты.

---
+++ Models/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace ServerABBKCOSY.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public int UserId { get; set; } // Связь с пользователем
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        // Тип адреса (доставка, оплата, домашний и т.д.)
+        public string AddressType { get; set; } // Например, "Shipping", "Billing"
+        // Навигационное свойство
+        public User User { get; set; }
+    }
+}
